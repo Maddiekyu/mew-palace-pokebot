31d4badcbd2a6f0f0e7da887f1323b1ca43d9c8b
Maddie Kyu <maddiekyu@protonmail.com>
2021-02-26 21:55:23 -0600

    added comment

--- rolebot.py
+++ rolebot.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 #Token for Discord Bot
 TOKEN = 'HsuNc7WBeJsgIBs9Vkz2ROO5q-e5WN4K'
 
-#Prefix for Discord will be %
+#Prefix for Discord will be %sh
+#Format: %sh Mimikyu
 client = commands.Bot(command_prefix='%')
 
 #run bot using specified token

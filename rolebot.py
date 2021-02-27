6dd8997ae49a31f796489d4e3d18165b9340032f
Maddie Kyu <maddiekyu@protonmail.com>
2021-02-26 21:28:15 -0600

    comments, removed useless code

--- rolebot.py
+++ rolebot.py
@@ -1,14 +1,11 @@
 import discord
 from discord.ext import commands
 
+#Token for Discord Bot
 TOKEN = ''
 
+#Prefix for Discord will be %
 client = commands.Bot(command_prefix='%')
 
-@client.command(pass_context=True)
-async def add_role(ctx):
-    member = ctx.author
-    role = discord.utils.get(member.guild.roles, name="Bots")
-    await member.add_roles(role)
-
+#run bot using specified token
 client.run(TOKEN)

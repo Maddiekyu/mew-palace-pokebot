df0965268636ee71921517418ea54ec966715e65
Maddie Kyu <maddiekyu@protonmail.com>
2021-02-27 22:51:09 -0600

    added full pokemon .json file up through Calyrex, including Alolan/Galarian forms

--- rolebot.py
+++ rolebot.py
@@ -1,38 +1,47 @@
 import discord
-import pypokedex
-import constant
+import pokebase as pb
 from discord.ext import commands
-from pypokedex import pokemon
+import json
 
+#convert JSON to dict
+with open('pokemon.json') as json_file:
+    pokemonData = json.load(json_file)
+
+print(type(pokemonData))
+# print("\nPokemon 1:", pokemonData['bulbasaur']) 
+# print("\nPokemon 2:", pokemonData['ivysaur']) 
 # For testing purposes only:
-userInput = input("Enter the Pokemon you want to shiny hunt: ")
-print(userInput)
+# userInput = input("Enter the Pokemon you want to shiny hunt: ")
+# print(userInput)
 
-# # Validate User Input Function
+# Validate User Input Function
 # def validate_user_input():
-#     userInput = message
-
-
-
+#     print("hello world")
 
 # Constants
-POKETWO_TOTAL = 809
+# POKETWO_TOTAL = 898
 
 # Token for Discord Bot
-TOKEN = 'HsuNc7WBeJsgIBs9Vkz2ROO5q-e5WN4K'
+# TOKEN = 'HsuNc7WBeJsgIBs9Vkz2ROO5q-e5WN4K'
 
 # Prefix for Discord will be %sh
 # Format: %sh Mimikyu
-client = commands.Bot(command_prefix='%')
+# client = commands.Bot(command_prefix='%')
 
 # Fill map with {pokedex #, pokemon} key value pairs
 # API call for Pokemon data.
 # https://pypi.org/project/pypokedex/
-pokemonNames = []
-index = 0
-for i in range(1, POKETWO_TOTAL):
-    pokemonNames.append(pypokedex.get(dex = i))
+# pokemonNames = []
+# index = 0
+# #pokeName = pypokedex.pokemon.Pokemon()
+# for i in range(1, POKETWO_TOTAL + 1):
+#     pokemonNames.append(pb.pokemon(i))
+#     print(pokemonNames[i - 1])
 
+# print(type(pokemonNames[1]))
+# print(pokemonNames[1])
+
+# pokeName = pypokedex.pokemon.Pokemon.name
 # Pseudocode
 # if message starts with "%sh"
 #   validate_user_input()
@@ -46,10 +55,10 @@ for i in range(1, POKETWO_TOTAL):
 #   throw an error 
 #   prompt the user to try again: correct format.  
 
-if userInput in pokemonNames:
-    print("Yes, it exists")
-else:
-    print("No, it does not exist")
+# if userInput in pokemonNames:
+#     print("Yes, it exists")
+# else:
+#     print("No, it does not exist")
 
 # @client.command(pass_context=True)
 # async def loot(ctx,*,message):
@@ -66,4 +75,4 @@ else:
 
 
 #run bot using specified token
-client.run(TOKEN)
+#client.run(TOKEN)

cc70257b4860ffa21cf189d486ba5c3f51dbbb96
Maddiekyu <nerdsnerdsnerdsx3@gmail.com>
2021-03-05 16:40:11 -0600

    properly adds and removes roles; need to limit users to only one pokemon named role

--- rolebot.py
+++ rolebot.py
@@ -21,49 +21,49 @@ async def on_ready():
         print('Bot is ready.')
 
 # sh add role command
-@client.command()
+@client.command(pass_context=True)
 async def sha(ctx):
     #parse user's message
-    @client.event
-    async def on_message(message):
-        #channel = message.channel
-        # split "%sha " from "[Pokemon Name]"
-        pkmnName = message.content[5:].lower()
-        #check if it is a valid pokemon name
-        pkmnExists = pkmnName in pkmnSet
-        def check(message): 
-            return pkmnExists
-        # if a role doesn't already exist, create it
-        # otherwise, just add the role to the
-        if pkmnExists:
-            if not get(ctx.guild.roles, name = pkmnName):
-                role = await ctx.guild.create_role(name=pkmnName)
-            else:
-                member = ctx.message.author
-                role = discord.utils.get(member.guild.roles, name=pkmnName)
-            await ctx.author.add_roles(role)
+    message = ctx.message
+    # split "%sha " from "[Pokemon Name]"
+    pkmnName = message.content[5:].lower()
+    #check if it is a valid pokemon name
+    pkmnExists = pkmnName in pkmnSet
+    def check(message): 
+        return pkmnExists
+    # if a role doesn't already exist, create it
+    # otherwise, just add the role to the
+    if pkmnExists:
+        if not get(ctx.guild.roles, name = pkmnName):
+            new_role = await ctx.guild.create_role(name=pkmnName)
+        member = ctx.message.author
+        new_role = discord.utils.get(member.guild.roles, name=pkmnName)
+        old_role = (get(ctx.guild.roles, name = n) for n in pkmnSet)
+        await ctx.send(f"{member.mention} is now hunting {new_role.mention}.")
+        if old_role in member.roles:
+            await ctx.author.remove_roles(member,*old_role)
+            await ctx.send(f"{member.mention} is no longer hunting {new_role.mention}.")
+        await member.add_roles(new_role)
 
-# sh add role command
-@client.command()
+# sh remove role command
+@client.command(pass_context=True)
 async def shr(ctx):
     #parse user's message
-    @client.event
-    async def on_message(message):
-        #channel = message.channel
-        # split "%shr " from "[Pokemon Name]"
-        pkmnName = message.content[5:].lower()
-        #check if it is a valid pokemon name
-        pkmnExists = pkmnName in pkmnSet
-        def check(message): 
-            return pkmnExists
-        # if a role doesn't already exist, create it
-        # otherwise, just add the role to the
-        if pkmnExists:
-            if not get(ctx.guild.roles, name = pkmnName):
-                role = await ctx.guild.create_role(name=pkmnName)
-            else:
-                member = ctx.message.author
-                role = discord.utils.get(member.guild.roles, name=pkmnName)
-            await ctx.author.remove_roles(role)
+    message = ctx.message
+    # split "%shr " from "[Pokemon Name]"
+    pkmnName = message.content[5:].lower()
+    #check if it is a valid pokemon name
+    pkmnExists = pkmnName in pkmnSet
+    def check(message): 
+        return pkmnExists
+    # if a role doesn't already exist, create it
+    # otherwise, just add the role to the
+    if pkmnExists:
+        if not get(ctx.guild.roles, name = pkmnName):
+            role = await ctx.guild.create_role(name=pkmnName)
+        member = ctx.message.author
+        role = discord.utils.get(member.guild.roles, name=pkmnName)
+        await ctx.send(f"{member.mention} is no longer hunting {role.mention}.")
+        await member.remove_roles(role)
             
 client.run(os.getenv('TOKEN'))

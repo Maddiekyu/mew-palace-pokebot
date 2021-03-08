920ee4f67ce9cfe977b8d7e184d780db737cbb99
Maddiekyu <nerdsnerdsnerdsx3@gmail.com>
2021-03-07 19:50:45 -0600

    added mute and unmute command in RaidHost.py

--- cogs/RaidHost.py
+++ cogs/RaidHost.py
@@ -16,7 +16,7 @@ class RaidHost(commands.Cog):
     async def on_ready(self):
         print('Raid Host Commands Loaded.')
 
-    @commands.command(pass_context=True)
+    @commands.command(name = 'host', pass_context = True, invoke_without_command = True)
     @commands.has_role("🌟Raid Host🌟")
     async def host(self, ctx):
         guild = ctx.message.guild
@@ -31,7 +31,7 @@ class RaidHost(commands.Cog):
         }
 
         # Create private channel.
-        channel = await guild.create_text_channel(raidChannelName, overwrites = overwrites, category = ctx.guild.categories[1])
+        hostChannel = await guild.create_text_channel(raidChannelName, overwrites = overwrites, category = ctx.guild.categories[1])
 
         # Welcome to shiny mimikyu
         # Following are the available bot commands that you can use. Please note that all commands must be executed from this channel.
@@ -52,6 +52,17 @@ class RaidHost(commands.Cog):
     async def host_error(self, ctx, error):
         if isinstance(error, commands.MissingRole):
             await ctx.send("Sorry, only Raid Hosts are allowed to use this command.")
+    
+    @commands.command(name = 'mute', pass_context = True)
+    async def mute(self, ctx, member: discord.Member):
+        await ctx.channel.set_permissions(member, send_messages=False, view_channel=True)
+        embed = discord.Embed(title="muted", description=f"{member.mention} was muted ", colour=discord.Colour.red())
+        await ctx.send(embed = embed)
 
+    @commands.command(name = 'unmute', pass_context = True)
+    async def unmute(self, ctx, member: discord.Member):
+        await ctx.channel.set_permissions(member, send_messages=True, view_channel=True)
+        embed = discord.Embed(title="unmuted", description=f"{member.mention} was unmuted ", colour=discord.Colour.green())
+        await ctx.send(embed = embed)
 def setup(client):
     client.add_cog(RaidHost(client))
